06f1fd64187e57fc961be850205eb77381e7cf87
Kate Chadova <Kity>
2022-12-17 21:45:33 +0300

    Исправила кодировку

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Box box = new Box();
-        Chocolates mars = new Chocolates("Марс", 35 , 24.0, "Батончик");
-        Marmalade flaf = new Marmalade("Флаф", 40.0, 35.4, "Черная смородина");
-        Candies ozera = new Candies("Озера", 125.0, 15.5, "Со сгущенкой");
-        Lolipops mix = new Lolipops("Микс", 5.0, 12, "киви");
+        Chocolates mars = new Chocolates("РјР°СЂСЃ", 35 , 24.0, "Р±Р°С‚РѕРЅС‡РёРє");
+        Marmalade flaf = new Marmalade("С„Р»Р°С„", 40.0, 35.4, "С‡РµСЂРЅР°СЏ СЃРјРѕСЂРѕРґРёРЅР°");
+        Candies ozera = new Candies("РѕР·РµСЂР°", 125.0, 15.5, "СЃРѕ СЃРіСѓС‰РµРЅРєРѕР№");
+        Lolipops mix = new Lolipops("РјРёРєСЃ", 5.0, 12, "СЏР±Р»РѕРєРѕ");
 
-        System.out.println("Сколько добавить шокладок?");
-        System.out.println("(Введите значение от 0 до 3)");
+        System.out.println("РЎРєРѕР»СЊРєРѕ С€РѕРєРѕР»Р°РґРѕРє РґРѕР±Р°РІРёС‚СЊ?");
+        System.out.println("(Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ РѕС‚ 0 РґРѕ 3)");
         Scanner sc = new Scanner(System.in);
         int amountChocolates = sc.nextInt();
         switch (amountChocolates){
@@ -29,8 +29,8 @@ public class Main {
                 break;
         }
 
-        System.out.println("Сколько добавить мармелада?");
-        System.out.println("(Введите значение от 0 до 3)");
+        System.out.println("РЎРєРѕР»СЊРєРѕ РјР°СЂРјРµР»Р°РґР° РґРѕР±Р°РІРёС‚СЊ?");
+        System.out.println("(Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ РѕС‚ 0 РґРѕ 3)");
         Scanner sc1 = new Scanner(System.in);
         int amountMarmelad = sc1.nextInt();
         switch (amountMarmelad){
@@ -50,8 +50,8 @@ public class Main {
                 break;
         }
 
-        System.out.println("Сколько добавить лединцов?");
-        System.out.println("(Введите значение от 0 до 3)");
+        System.out.println("РЎРєРѕР»СЊРєРѕ Р»РµРґРёРЅС†РѕРІ РґРѕР±Р°РІРёС‚СЊ?");
+        System.out.println("(Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ РѕС‚ 0 РґРѕ 3)");
         Scanner sc2 = new Scanner(System.in);
         int amountLolipop = sc2.nextInt();
         switch (amountLolipop){
@@ -71,8 +71,8 @@ public class Main {
                 break;
         }
 
-        System.out.println("Сколько добавить упаковок конфет?");
-        System.out.println("(Введите значение от 0 до 3)");
+        System.out.println("РЎРєРѕР»СЊРєРѕ СѓРїРѕРєРѕРІРѕРє РєРѕРЅС„РµС‚ РґРѕР±Р°РІРёС‚СЊ?");
+        System.out.println("(Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ РѕС‚ 0 РґРѕ 3)");
         Scanner sc3 = new Scanner(System.in);
         int amountCandies = sc3.nextInt();
         switch (amountCandies){
@@ -93,41 +93,41 @@ public class Main {
         }
 
 while (true) {
-    System.out.println("Хотите выполнить дополнительные функции: удаление сладости, узнать вес, узнать стоимость, оптимизация по весу, оптимизация по стоимости");
-    System.out.println("(Введите: нет/ удалить/ удалить по индексу/ вес/ стоимость/ информация о наполнении коробки/  оптимизация по весу / оптимизация по стоимости)");
+    System.out.println("РҐРѕС‚РёС‚Рµ РёСЃРїРѕР»СЊР·РѕРІР°С‚СЊ РґРѕРї С„СѓРЅРєС†РёСЋ: СѓРґР°Р»РёС‚СЊ РїРѕСЃР»РµРґРЅРёР№, СѓРґР°Р»РёС‚СЊ РїРѕ РёРЅРґРµРєСЃСѓ, СѓР·РЅР°С‚СЊ РІРµСЃ, СѓР·РЅР°С‚СЊ С†РµРЅСѓ, СѓР·РЅР°С‚СЊ СЃРѕРґРµСЂР¶Р°РЅРёРµ РєРѕСЂРѕР±РєРё, РѕРїС‚РёРјРёР·Р°С†РёСЏ РїРѕ РІРµСЃСѓ, РѕРїС‚РёРјРёР·Р°С†РёСЏ РїРѕ СЃС‚РѕРёРјРѕСЃС‚Рё");
+    System.out.println("(Р’РІРµРґРёС‚Рµ: РЅРµС‚ / СѓРґР°Р»РёС‚СЊ РїРѕСЃР»РµРґРЅРёР№ / СѓРґР°Р»РёС‚СЊ РїРѕ РёРЅРґРµРєСЃСѓ / РІРµСЃ / С†РµРЅР° / СѓР·РЅР°С‚СЊ СЃРѕРґРµСЂР¶Р°РЅРёРµ РєРѕСЂРѕР±РєРё / РѕРїС‚РёРјРёР·Р°С†РёСЏ РїРѕ РІРµСЃСѓ / РѕРїС‚РёРјРёР·Р°С†РёСЏ РїРѕ СЃС‚РѕРёРјРѕСЃС‚Рё)");
     Scanner sc4 = new Scanner(System.in);
     String comands = sc4.nextLine();
     switch (comands) {
-        case "нет":
+        case "РЅРµС‚":
             break;
-        case "удалить":
+        case "СѓРґР°Р»РёС‚СЊ РїРѕСЃР»РµРґРЅРёР№":
             box.deleteLast();
-            System.out.println("Последний добавленный товар удален");
+            System.out.println("РџРѕСЃР»РµРґРЅРёР№ СЌР»РёРјРµРЅС‚ СѓРґР°Р»РµРЅ");
             break;
-        case "удалить по индексу":
-            System.out.println("Введите индекс");
+        case "СѓРґР°Р»РёС‚СЊ РїРѕ РёРЅРґРµРєСЃСѓ":
+            System.out.println("Р’РІРµРґРёС‚Рµ РёРЅРґРµРєСЃ");
             Scanner sc5 = new Scanner(System.in);
             int ind = sc5.nextInt();
             box.deleteIndex(ind);
-            System.out.println("Товар с индексом " + ind + " удален");
+            System.out.println("Р­Р»РёРјРµРЅС‚ СЃ РёРЅРґРµРєСЃРѕРј " + ind + " СѓРґР°Р»РµРЅ");
             break;
-        case "вес":
+        case "РІРµСЃ":
             box.weigth();
             break;
-        case "стоимость":
+        case "С†РµРЅР°":
             box.price();
             break;
-        case "информация о наполнении коробки":
+        case "СѓР·РЅР°С‚СЊ СЃРѕРґРµСЂР¶Р°РЅРёРµ РєРѕСЂРѕР±РєРё":
             box.info();
             break;
-        case "оптимизация по весу":
-            System.out.println("Введите максимальный вес");
+        case "РѕРїС‚РёРјРёР·Р°С†РёСЏ РїРѕ РІРµСЃСѓ":
+            System.out.println("Р’РІРµРґРёС‚Рµ РјР°РєСЃРёРјР°Р»СЊРЅС‹Р№ РІРµСЃ");
             Scanner sc6 = new Scanner(System.in);
             double maxWeight = sc6.nextDouble();
             box.optimizationWeight(maxWeight);
             break;
-        case "оптимизация по стоимости":
-            System.out.println("Введите максимальную стоимость");
+        case "РѕРїС‚РёРјРёР·Р°С†РёСЏ РїРѕ СЃС‚РѕРёРјРѕСЃС‚Рё":
+            System.out.println("Р’РІРµРґРёС‚Рµ РјР°РєСЃРёРјР°Р»СЊРЅСѓСЋ СЃС‚РѕРёРјРѕСЃС‚СЊ");
             Scanner sc7 = new Scanner(System.in);
             double maxPrice = sc7.nextDouble();
             box.optimizationPrice(maxPrice);
